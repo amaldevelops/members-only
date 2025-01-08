6424f70526f175b1b5eb68413a95da87b060b8f0
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-08 18:47:40 +1100

    Add SQL code to populatedb

--- db/populatedb.js
+++ db/populatedb.js
@@ -5,17 +5,50 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS members_only
+CREATE TABLE IF NOT EXISTS users_table(
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+first_name VARCHAR(100),
+last_name VARCHAR(300),
+username VARCHAR(300),
+password VARCHAR(100) NOT NULL,
+membership_status VARCHAR(100),
+admin BOOLEAN DEFAULT FALSE
+);
+INSERT INTO users_table(
+first_name,last_name,username,password,membership_status,admin)
+VALUES
+(
+'Jean Luc',
+'Picard',
+'jeanluc@startrek.universe',
+'20252025',
+'member',
+'TRUE'
+);
+
+CREATE TABLE IF NOT EXISTS messages(
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+title VARCHAR(200),
+timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
+content VARCHAR(1000),
+author_id INTEGER NOT NULL,
+FOREIGN KEY (author_id) REFERENCES users_table(id)ON DELETE CASCADE
+);
 
 
-`;
+INSERT INTO messages(title,content,author_id)
+VALUES
+(
+'CASIO watches are the best',
+'I find that CASIO watches are durable and accurate and as a StarShip captain who travels to different planets I find them to be awesome !',
+'jeanluc@startrek.universe'
+);`;
 
 async function main() {
   console.log("Seeding data to database....");
 
   const client = new Client({
-    connectionString: `${process.env.URI_PRODUCTION}`,
+    connectionString: `${process.env.URI_LOCAL_MACHINE}`,
     ssl: {
       rejectUnauthorized: false,
     },
