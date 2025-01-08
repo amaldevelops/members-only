54edec3873824175d21d554d5a24613f057072bc
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-08 16:56:12 +1100

    Add JS code to populatedb

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,5 +1,31 @@
 #! /usr/bin/env node
 
-const {Client} =require("pg");
+const { Client } = require("pg");
 
 require("dotenv").config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS members_only
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+
+
+`;
+
+async function main() {
+  console.log("Seeding data to database....");
+
+  const client = new Client({
+    connectionString: `${process.env.URI_PRODUCTION}`,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+
+  console.log("Database Populated successfully !");
+}
+
+main();
