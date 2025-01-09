85fd4547f5b88ed4e326d1c58f3912ce389b201a
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-09 12:02:10 +1100

    Amended populatedb SQL code to include author_id

--- db/populatedb.js
+++ db/populatedb.js
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users_table(
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 first_name VARCHAR(100),
 last_name VARCHAR(300),
-username VARCHAR(300),
+username VARCHAR(300) UNIQUE NOT NULL,
 password VARCHAR(100) NOT NULL,
 membership_status VARCHAR(100),
 admin BOOLEAN DEFAULT FALSE
@@ -24,24 +24,27 @@ VALUES
 '20252025',
 'member',
 'TRUE'
-);
+)
+ON CONFLICT (username) DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS messages(
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 title VARCHAR(200),
 timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
 content VARCHAR(1000),
+author_name VARCHAR(300),
 author_id INTEGER NOT NULL,
-FOREIGN KEY (author_id) REFERENCES users_table(id)ON DELETE CASCADE
+FOREIGN KEY (author_id) REFERENCES users_table(id) ON DELETE CASCADE
 );
 
 
-INSERT INTO messages(title,content,author_id)
+INSERT INTO messages(title,content,author_name,author_id )
 VALUES
 (
 'CASIO watches are the best',
 'I find that CASIO watches are durable and accurate and as a StarShip captain who travels to different planets I find them to be awesome !',
-'jeanluc@startrek.universe'
+'Jean Luc',
+1
 );`;
 
 async function main() {
